81e63ce7ed4f820674e208eb266fb9f37cded749
Zixuan Zeng <zzeng@ucsd.edu>
2025-12-05 14:36:09 -0800

    Add some more classes

---
+++ Class04/Class04.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "Class 4 lab"
+#' author: "Zixuan Zeng (PID: A16142927)"
+#' date: "10/10/2025"
+#' ---
+
+#My_first_R_script
+x <- 1:50
+plot(x)
+plot(x,col="blue")
+plot(x,sin(x),typ="l",col="red",lwd=3)
+
+log(10)
+log(10,base=10)
+log(10,2)
